cb82e48a2f9188baccbdc1f51d5b38448f1b72c4
avsessisozx <129687270+avsessisozx@users.noreply.github.com>
2026-05-19 23:29:16 +0300

    Add initial Android build.gradle.kts configuration

---
+++ android/build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    kotlin("android") version "1.8.22" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
